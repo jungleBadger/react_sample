df212fc662f3c92809de73d360c3b43b21385b28
Daniel Abrao <danielpaladar@gmail.com>
2018-02-10 16:43:41 -0200

    Add classList polyfill

--- client/vanilla_module/js/main.js
+++ client/vanilla_module/js/main.js
@@ -2,6 +2,7 @@
     "use strict";
 
 	require("shim-keyboard-event-key");
+	require("classlist-polyfill");
 	if (!Array.prototype.find) {
 		Array.prototype.find = require("../../etc/shared_libs/polyfills").find;
 	}
